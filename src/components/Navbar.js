7134724ebae0675712c86693280015cfb6a4d36c
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-06-03 03:29:00 +0530

    change title name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Menu from "./Menu";
 function Navbar() {
   return (
     <div className="container navbar">
-      <h5 style={{ cursor: "pointer" }}>Course builder</h5>
+      <h5 style={{ cursor: "pointer" }}>Course build</h5>
       <Menu />
     </div>
   );
